70cb6d58f2c7b885115252078cde26123a778fee
ssmanian07 <ssmanianmca@gmail.com>
2020-04-04 19:54:44 +0530

    Get CustomerData from Database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -9,17 +9,28 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
 
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id) 
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null) 
             {
@@ -28,14 +39,6 @@ namespace Vidly.Controllers
 
             return View(customer);
         }
-
-        public IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-               new Customer { Id = 1, Name = "John Smith" },
-               new Customer { Id = 2, Name = "Mary Williams" }
-            };
-        }
+         
     }
 }
