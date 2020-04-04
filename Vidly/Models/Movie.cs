dde8262dafa8fc745b79bbdd17c4173dc7b4729a
ssmanian07 <ssmanianmca@gmail.com>
2020-04-04 22:56:30 +0530

    Display the Lis of Movies and their Genre from database

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,8 +1,22 @@
-﻿namespace Vidly.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
+        public byte GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public byte NumberInStock { get; set; }
     }
 }
